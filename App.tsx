bdfcfb1728f2e75817e2b7ee064363ae68ccebcc
Lucas Sales <lucassales398@gmail.com>
2021-06-24 12:07:57 -0300

    Autenticação e contexto

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, LogBox } from "react-native";
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import {
@@ -10,6 +10,12 @@ import AppLoading from "expo-app-loading";
 
 import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
+
+import { AuthProvider } from "./src/hooks/auth";
+
+LogBox.ignoreLogs([
+  "You are not currently signed in to Expo on your development machine.",
+]);
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +36,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </Background>
   );
 }
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../hooks/auth";
 
-import {Avatar} from '../Avatar'
+import { Avatar } from "../Avatar";
 
 import { styles } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-        <Avatar urlImage="https://github.com/lucas-salles.png" />
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
 
-          <Text style={styles.username}>Lucas</Text>
+          <Text style={styles.username}>{user.firstName}</Text>
         </View>
 
         <Text style={styles.message}>Hoje é dia de vitória</Text>
